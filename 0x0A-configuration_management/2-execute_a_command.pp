dac20da8eb2fb9870a26cb10301c9313d83b9747
LusterPearl <salamioluwatosin90@gmail.com>
2023-12-22 03:52:54 -0800

    add

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,13 @@
 # Puppet manifest to kill a process named killmenow
-exec { 'pkill':
-  command => 'pkill killmenow',
-  provider => shell,
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  refreshonly => true,
+  path        => ['/bin', '/usr/bin'],
+}
+
+# Trigger the exec resource only if the process needs to be killed
+if $::osfamily == 'Debian' {
+  notify { 'Kill killmenow process':
+    require => Exec['killmenow'],
+  }
 }
