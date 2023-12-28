93673c866c1c12df15f9a57c1a8a4286594e3f01
LusterPearl <salamioluwatosin90@gmail.com>
2023-12-28 05:28:56 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,6 @@
 # Puppet manifest to configure SSH client
 
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#PasswordAuthentication',
-  replace => 'PasswordAuthentication',
-}
-
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^#IdentityFile',
-  replace => 'IdentityFile',
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "# SSH client configuration file\n\nHost *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
 }
