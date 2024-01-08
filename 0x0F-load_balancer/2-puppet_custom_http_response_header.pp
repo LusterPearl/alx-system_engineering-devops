9e01985a93e9691869cf1f07f4efe9435730d593
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-08 05:30:37 -0800

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,43 +1,26 @@
-# 2-puppet_custom_http_response_header.pp
-
 # Install Nginx package
-package { 'nginx':
-  ensure => installed,
+class { 'nginx':
+  ensure => 'installed',
 }
 
-# Define a custom fact to get the server's hostname
-Facter.add('server_hostname') do
-  setcode 'hostname'
-end
+# Get the hostname
+$hostname = $facts['networking']['hostname']
 
-# Configure Nginx with a custom HTTP header
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "# Puppet Managed\nserver {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        add_header X-Served-By $server_hostname;
-        root   /var/www/html;
-        index  index.html index.htm;
-    }
-}",
-  notify  => Service['nginx'],
+# Create a custom HTTP response header
+file { '/etc/nginx/sites-available/custom_header':
+  ensure  => 'file',
+  content => "add_header X-Served-By ${hostname};",
 }
 
-# Enable the default site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
+# Symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/custom_header':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/custom_header',
 }
 
-# Restart Nginx service when the configuration changes
+# Restart Nginx for changes to take effect
 service { 'nginx':
-  ensure    => running,
+  ensure    => 'running',
   enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+  subscribe => File['/etc/nginx/sites-available/custom_header'],
 }
