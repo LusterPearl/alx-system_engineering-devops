198d30a7ce63272235bdb601c34c9a6429ac7674
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-30 04:41:41 -0800

    task3done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,64 +1,38 @@
 #!/usr/bin/python3
-"""
-Extend your Python script to export data in the JSON format.
-"""
-
+'''A script that retrieves data from an API and exports it to a JSON file.'''
 import json
 import requests
-from sys import argv
-from os.path import isfile
 
+API_URL = 'https://jsonplaceholder.typicode.com'
+"""The base URL of the JSONPlaceholder API."""
 
-def read_existing_data(filename):
-    if isfile(filename):
-        try:
-            with open(filename, 'r') as file:
-                return json.load(file)
-        except json.JSONDecodeError:
-            return {}
-    return {}
+if __name__ == '__main__':
+    # Make an API call to retrieve users' data
+    users_res = requests.get('{}/users'.format(API_URL)).json()
+    # Make an API call to retrieve todos' data
+    todos_res = requests.get('{}/todos'.format(API_URL)).json()
+    # Initialize a dictionary to store users' data
+    users_data = {}
 
+    # Iterate through each user
+    for user in users_res:
+        # Extract user ID and username
+        user_id = user.get('id')
+        user_name = user.get('username')
+        # Filter todos associated with the user
+        user_todos = list(filter(lambda x: x.get('userId') == user_id, todos_res))
+        # Prepare user's data
+        user_data = list(map(
+            lambda x: {
+                'username': user_name,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            },
+            user_todos
+        ))
+        # Store user's data in the dictionary
+        users_data['{}'.format(user_id)] = user_data
 
-if __name__ == "__main__":
-    # Check if an employee ID is provided as a command-line argument
-    if len(argv) == 2 and argv[1].isdigit():
-        employee_id = argv[1]
-    else:
-        print("Usage: {} EMPLOYEE_ID".format(argv[0]))
-        exit(1)
-
-    # Construct user URL and fetch employee data from the JSONPlaceholder API
-    user_url = (
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    )
-    response_user = requests.get(user_url)
-    user_data = response_user.json()
-    employee_username = user_data.get("username")
-
-    # Construct todos URL and fetch todos data for the specific employee
-    todos_url = (
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(employee_id)
-    )
-    response_todos = requests.get(todos_url)
-    todos_data = response_todos.json()
-
-    # Create a dictionary with the required format
-    new_employee_data = {employee_id: []}
-    for todo in todos_data:
-        task_dict = {
-            "username": employee_username,
-            "task": todo["title"],
-            "completed": todo["completed"]
-        }
-        new_employee_data[employee_id].append(task_dict)
-
-    # Read existing data from the file
-    existing_data = read_existing_data("todo_all_employees.json")
-
-    # Update the existing data with the new employee data
-    existing_data.update(new_employee_data)
-
-    # Export the updated data to todo_all_employees.json
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(existing_data, json_file, indent=2)
+    # Write the JSON data to a file
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(users_data, json_file)
