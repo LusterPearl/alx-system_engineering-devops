bcfbbd4e2555149f32d044b99176dad1cb4f09a5
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-30 04:52:05 -0800

    task3solved

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-'''A script that retrieves data from an API and exports it to a JSON file.'''
+"""
+Retrieves data from an API and exports it to a JSON file
+"""
 import json
 import requests
 
 API_URL = 'https://jsonplaceholder.typicode.com'
-"""The base URL of the JSONPlaceholder API."""
+"""The URL of the JSONPlaceholder """
+
 
 if __name__ == '__main__':
-    # Make an API call to retrieve users' data
+    # Retrieve users data using API
     users_res = requests.get('{}/users'.format(API_URL)).json()
-    # Make an API call to retrieve todos' data
+    # Retrieve todos data using API
     todos_res = requests.get('{}/todos'.format(API_URL)).json()
-    # Initialize a dictionary to store users' data
+    # Start a dictionary to store users' data
     users_data = {}
 
-    # Iterate through each user
+    # Go through each user
     for user in users_res:
-        # Extract user ID and username
+        # Collect a user ID and username
         user_id = user.get('id')
         user_name = user.get('username')
         # Filter todos associated with the user
-        user_todos = list(filter(lambda x: x.get('userId') == user_id, todos_res))
-        # Prepare user's data
+        user_todos = list(filter(lambda x: x.get('userId') == user_id,
+                                 todos_res))
+        # Start user data
         user_data = list(map(
             lambda x: {
                 'username': user_name,
@@ -30,7 +34,7 @@ if __name__ == '__main__':
             },
             user_todos
         ))
-        # Store user's data in the dictionary
+        # Store data in the dictionary
         users_data['{}'.format(user_id)] = user_data
 
     # Write the JSON data to a file
