eb22c7d47aa72280a097502df017b3272c3436db
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-14 04:15:43 -0800

    11

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,16 @@
+# 0-strace_is_your_friend.pp
+# Puppet manifest to fix Apache 500 error using strace debugging
+
+# Execute command to fix the issue
+exec { 'fix-wordpress':
+  command     => '/bin/some_command_to_fix_the_issue',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+}
+
+# Notify Apache service to restart if the fix is applied
+service { 'apache2':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['fix-wordpress'],
+}
