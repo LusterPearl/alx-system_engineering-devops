a97f6c8181e88ed89500ccafd20d83f04a64a183
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-14 13:19:44 -0800

    lll

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,12 @@
-# fix Apache 500 error and corrects a typo in wordpress settings and fixes 500
-exec { 'fix-apache':
-command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-path => ['/bin', '/usr/bin'],
+# fix Apache 500 error and corrects a typo in wordpress settings and fixes 50
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php')
+              content.gsub('phpp', 'php'),
+}
+
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/var/www/html/wp-settings.php'],
 }
