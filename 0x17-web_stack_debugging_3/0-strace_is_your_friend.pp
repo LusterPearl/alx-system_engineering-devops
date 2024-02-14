575e1e7169202a28ea4fa48b1479ba623c80211e
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-14 07:00:03 -0800

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,5 @@
-# 0-strace_is_your_friend.pp
-# Puppet manifest to fix Apache 500 error using strace debugging
-
-# Execute command to fix the issue
-exec { 'fix-apache-error':
-  command     => '/bin/some_command_to_fix_the_issue',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-}
-
-# Notify Apache service to restart if the fix is applied
-service { 'httpd':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['fix-apache-error'],
+# Puppet manifest to fix Apache 500 error using puppet
+exec { 'fix-apache':
+command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+path => ['/bin', '/usr/bin'],
 }
