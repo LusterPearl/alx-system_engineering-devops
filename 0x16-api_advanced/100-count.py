23198aa967ca554dfcabbc008398d07ff6006e31
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-14 01:16:24 -0800

    1

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-100-count aicles
+Module to interact with Reddit's API
 """
 
-import json
 import requests
 
 
@@ -11,11 +10,19 @@ def count_words(subreddit, word_list, after=None, word_count={}):
     """
     Recursively queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords.
-    """
-    if count is None:
-        count = {}
+    Args:
+        subreddit (str): The name of the subreddit.
+        word_list (list): List of keywords to count.
+        after (str): The 'after' parameter for pagination.
+        word_count (dict): Dictionary to store word counts.
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    Returns:
+        None
+    """
+    if word_count is None:
+        word_count = {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'bhalut'}
     params = {'after': after} if after else {}
 
@@ -27,17 +34,19 @@ def count_words(subreddit, word_list, after=None, word_count={}):
         for post in data['children']:
             title = post['data']['title'].lower()
             for word in word_list:
-                count[word] = count.get(word, 0) + title.count(word.lower())
+                word_count[word] = word_count.get(
+                                    word, 0) + title.count(word.lower())
 
         after = data['after']
         if after:
-            count_words(subreddit, word_list, after, count)
+            count_words(subreddit, word_list, after, word_count)
         else:
             sorted_count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
-            for word, word_count in sorted_count:
-                if word_count > 0:
-                    print(f"{word}: {word_count}")
+            for word, count in sorted_count:
+                if count > 0:
+                    print(f"{word}: {count}")
 
     else:
         print(f"Failed to fetch data for subreddit '{subreddit}':
+              {response.status_code")
               {response.status_code}")
